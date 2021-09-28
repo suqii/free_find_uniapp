3e22780ca50a9361016b8e44fd39d95a45ab47ea
wukui <3128006406@qq.com>
2021-09-28 10:21:57 +0800

    fix: "配置更改"

--- common/config.js
+++ common/config.js
@@ -1,6 +1,7 @@
 export default {
 	// api请求前缀
-	webUrl:'https://bt.wktest.cn/api/v1',
+	webUrl:'http://api.test2.com/api/v1',
+	// webUrl:'https://bt.wktest.cn/api/v1',
 	// websocket地址
 	websocketUrl:"ws://47.108.192.147:23481",
 }
